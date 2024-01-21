ad96ecb27d3cc2c4ad436cd3317c8d11a467803f
santipvz <santivzqzz@gmail.com>
2024-01-22 00:11:05 +0100

    Delete src/packages directory

--- src/packages/Transformers/abstract_transformer.py
+++
@@ -1,9 +0,0 @@
-'''Abstract class for sequence transformers.'''
-# pylint: disable=too-few-public-methods
-from abc import ABC, abstractmethod
-
-class AbstractTransformer(ABC):
-    '''Abstract class for sequence transformers.'''
-    @abstractmethod
-    def transform(self, sequence):
-        '''Returns a list of Sequence objects with the transformation applied.'''
